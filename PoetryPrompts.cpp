1738530a89688278fab9c0a31afbb4d1c64265e5
Sage Andromeda H. <143134634+SageAndromeda@users.noreply.github.com>
2026-07-21 22:04:13 -0400

    Random numbers
    Allowed some prompts to print a random number 1-5

--- PoetryPrompts.cpp
+++ PoetryPrompts.cpp
@@ -39,69 +39,64 @@ int main()
     // I'm using these to add features to certain prompts
     int defaultPrompt = 0;
     int notFirst = 1;
-
-    ///////////////////////////////////////////////////////////////////////////////
-    // Testing!!! Please remove later!
-    // pair <vector<int>, char> testPair;
-    // vector<int> testVector = {2,3};
-    // testPair = make_pair(testVector, 'a');
-    ///////////////////////////////////////////////////////////////////////////////
+    int numOneToFive = 2;
 
     // Preloaded prompts:
     // Kept in vectors with string, int pairs to prevent repetition
     // Consider keeping these in a function to allow easier repetition of program
     // Pairs with 0 are default and used as is
     // Pairs with 1 are user prompts and get exra weight
-    promptList.push_back(make_pair("Use two metaphors", defaultPrompt));
-    promptList.push_back(make_pair("Use a metaphor", defaultPrompt));
     promptList.push_back(make_pair("Write about your favorite item", defaultPrompt));
     promptList.push_back(make_pair("Write the first fun fact that comes to mind", defaultPrompt));
     promptList.push_back(make_pair("Lie", defaultPrompt));
-    promptList.push_back(make_pair("Write about a time spent with a friend", defaultPrompt));
+    promptList.push_back(make_pair("Write about a friend", defaultPrompt));
     promptList.push_back(make_pair("Tell the truth. About anything", defaultPrompt));
     promptList.push_back(make_pair("Mention a work of art and/or an artist", defaultPrompt));
     promptList.push_back(make_pair("Mention an author; they don't have to be famous", defaultPrompt));
     promptList.push_back(make_pair("Write about someone you consider family. You don't have to be related", defaultPrompt));
-    promptList.push_back(make_pair("Write four lines where the last word of each rhymes", defaultPrompt));
     promptList.push_back(make_pair("Use a gerund", defaultPrompt));
-    promptList.push_back(make_pair("Use the subject line of a recent email", defaultPrompt));
+    promptList.push_back(make_pair("Use the subject line of an email you received", defaultPrompt));
     promptList.push_back(make_pair("Use personification", defaultPrompt));
     promptList.push_back(make_pair("Describe your gender abstractly", defaultPrompt));
-    promptList.push_back(make_pair("Complete the phrase \"_____ is stronger than \"_____\"", defaultPrompt));
+    promptList.push_back(make_pair("Complete the phrase _____ is stronger than _____", defaultPrompt));
     promptList.push_back(make_pair("Write about summer", defaultPrompt));
     promptList.push_back(make_pair("Make up something about someone near you", defaultPrompt));
     promptList.push_back(make_pair("Only use words that start with letters in your first name", defaultPrompt));
     promptList.push_back(make_pair("Think of a random word. Now use that as the first word in this line", defaultPrompt));
-    promptList.push_back(make_pair("Write about a sport", defaultPrompt));
+    promptList.push_back(make_pair("Mention a sport", defaultPrompt));
     promptList.push_back(make_pair("State a fact about a favorite character", defaultPrompt));
     promptList.push_back(make_pair("Ask someone for a random word to use", defaultPrompt));
     promptList.push_back(make_pair("Only use words starting with letters from your last name", defaultPrompt));
     promptList.push_back(make_pair("Choose a random noun to use", defaultPrompt));
     promptList.push_back(make_pair("Choose a random adjective to use", defaultPrompt));
     promptList.push_back(make_pair("Choose a random verb to use", defaultPrompt));
-    promptList.push_back(make_pair("Choose a random number to use", defaultPrompt));
     promptList.push_back(make_pair("Choose a random adverb to use", defaultPrompt));
     promptList.push_back(make_pair("Use a pronoun", defaultPrompt));
-    promptList.push_back(make_pair("Only use words you already have in your poem", notFirst));
     promptList.push_back(make_pair("Mention a day of the week", defaultPrompt));
     promptList.push_back(make_pair("Mention an animal", defaultPrompt));
-    promptList.push_back(make_pair("Write about someone you haven't talked to in a long time", defaultPrompt));
+    promptList.push_back(make_pair("Mention a place you like", defaultPrompt));
+    promptList.push_back(make_pair("Write about someone you haven't interacted with in a long time", defaultPrompt));
     promptList.push_back(make_pair("Use an idiom", defaultPrompt));
     promptList.push_back(make_pair("Write about a mundane moment", defaultPrompt));
-    promptList.push_back(make_pair("Use a line from your favorite song", defaultPrompt));
+    promptList.push_back(make_pair("Use a line from a song", defaultPrompt));
     promptList.push_back(make_pair("Use alliteration", defaultPrompt));
     promptList.push_back(make_pair("Use a simile", defaultPrompt));
     promptList.push_back(make_pair("Use an onomatopoeia", defaultPrompt));
     promptList.push_back(make_pair("Use a rhyme", defaultPrompt));
-    promptList.push_back(make_pair("Use two similes", defaultPrompt));
     promptList.push_back(make_pair("Use a simile and a metaphor", defaultPrompt));
-    promptList.push_back(make_pair("Write a different line of the poem in reverse", notFirst));
     promptList.push_back(make_pair("Use your least favorite color", defaultPrompt));
     promptList.push_back(make_pair("Use repetition", defaultPrompt));
     promptList.push_back(make_pair("Describe something you can't currently see", defaultPrompt));
     promptList.push_back(make_pair("Use a hyperbole", defaultPrompt));
-    promptList.push_back(make_pair("Repeat a different line in your poem, but change one or two words", defaultPrompt));
-    promptList.push_back(make_pair("Personify your favorite food", defaultPrompt));
+    promptList.push_back(make_pair("Mention your favorite food", defaultPrompt));
+    promptList.push_back(make_pair("Only use words you already have in your poem", notFirst));
+    promptList.push_back(make_pair("Repeat a different line in your poem, but change one or two words", notFirst));
+    promptList.push_back(make_pair("Write a different line of the poem in reverse", notFirst));
+    promptList.push_back(make_pair("Use at least this many metaphors in your poem (with one at this line):", numOneToFive)); // Can combine with first one. Add code for prompts with numbers
+    promptList.push_back(make_pair("Write at least this many lines where the last word rhymes:", numOneToFive));
+    promptList.push_back(make_pair("Use at least this many similes in your poem (with one at this line):", numOneToFive));
+    promptList.push_back(make_pair("Use this number:", numOneToFive));
+    
 
     // Default number of prompts:
     int numPrompts = promptList.size();
@@ -198,8 +193,10 @@ int printPrompt(const vector<pair<string, int>>& promptList, const int& numPrint
         // Codes for what to print:
         // -1 = user input
         // 0 = default
-        // 1 = Do NOT use first
-        // 2 = N/A
+        // 1 = Cannot be the first one printed
+        // 2 = Random number 1-5
+        int minCode = -1;
+        int maxCode = 2;
 
         aPrompt = rand() % (promptList.size());
 
@@ -217,10 +214,18 @@ int printPrompt(const vector<pair<string, int>>& promptList, const int& numPrint
         if (numPrinted == 1 && promptList[aPrompt].second == 1){
             canPrint = false;
         }
+        // Code 2 picks a random number 1-5
+        // We then print and end the function early
+        // because this one has to change how the prompt is printed
+        if (promptList[aPrompt].second == 2) {
+            int randNum = (rand() % 5) + 1;
+            cout << (numPrinted) << ". " << promptList[aPrompt].first << " " << randNum << endl;
+            return aPrompt;
+        }
 
         // If invalid code, let it know, but still try printing anyways
         // Useful for debugging
-        if (promptList[aPrompt].second < -1 || promptList[aPrompt].second > 1) {
+        if (promptList[aPrompt].second < minCode || promptList[aPrompt].second > maxCode) {
             cout << "WARNING! INVALID INPUT CODE " << promptList[aPrompt].second << endl;
             canPrint = true;
         }
